4536909ceb16ecf0ac11d46f1dc477eedc91d1d8
Dhruv-Panwar-0 <panwardhruv86@gmail.com>
2023-08-16 18:12:57 +0530

    Cors support added

--- LamaBackend/src/main/java/com/wellsfargo/LamaBackend/controllers/ItemController.java
+++ LamaBackend/src/main/java/com/wellsfargo/LamaBackend/controllers/ItemController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -22,6 +23,7 @@ import com.wellsfargo.LamaBackend.entities.Item;
 import com.wellsfargo.LamaBackend.service.impl.ItemServiceImpl;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/item")
 public class ItemController {
 
--- LamaBackend/src/main/java/com/wellsfargo/LamaBackend/controllers/LoanCardController.java
+++ LamaBackend/src/main/java/com/wellsfargo/LamaBackend/controllers/LoanCardController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,6 +22,7 @@ import com.wellsfargo.LamaBackend.entities.LoanCard;
 import com.wellsfargo.LamaBackend.service.impl.LoanCardServiceImpl;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/loan")
 public class LoanCardController {
 
