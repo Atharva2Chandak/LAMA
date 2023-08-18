072c71da35a4fe33c4d22cc46bb72824dc3294c3
Dhruv-Panwar-0 <panwardhruv86@gmail.com>
2023-08-18 18:55:26 +0530

    Added swagger for api documentation

--- LamaBackend/src/main/java/com/wellsfargo/LamaBackend/LamaBackendApplication.java
+++ LamaBackend/src/main/java/com/wellsfargo/LamaBackend/LamaBackendApplication.java
@@ -9,7 +9,14 @@ import org.springframework.context.annotation.Bean;
 
 import com.wellsfargo.LamaBackend.databaseSpecifics.DatabaseConnectionWithDriverManager;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class LamaBackendApplication {
 
 	public static void main(String[] args) {
@@ -35,5 +42,14 @@ public class LamaBackendApplication {
 	public ModelMapper modelMapper() {
 		return new ModelMapper(); 
 	}
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
 
 }
